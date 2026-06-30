28947e800d5d764221845bdbf30b93d120b3a80a
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-01 07:36:26 +0800

    Update ForgotPassword.php

---
+++ packages/bit-es/siapa/src/Pages/Auth/ForgotPassword.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bites\Identity\Pages\Auth;
+
+use Filament\Pages\Auth\PasswordReset\RequestPasswordReset as BaseRequest;
+use Filament\Forms\Form;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Validation\ValidationException;
+
+class CustomForgotPassword extends BaseRequest
+{
+    public function form(Form $form): Form
+    {
+        return $form->schema([
+            TextInput::make('email')->email()->required()->exists('users')->live(onBlur: true),
+            TextInput::make('totp_code')->numeric()->length(6)->required()->visible(fn ($get) => filled($get('email')))
+                ->rule(function ($attr, $val, $fail) {
+                    $user = \App\Models\User::where('email', $this->data['email'])->firstOrFail();
+                    if (!$user->verifyTwoFactorCode($val)) $fail('Invalid TOTP code');
+                })
+        ]);
+    }
+}
