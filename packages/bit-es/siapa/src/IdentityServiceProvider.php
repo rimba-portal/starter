eede538daf653e3d1246f740f6db70ec967b851f
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-01 07:33:18 +0800

    Update IdentityServiceProvider.php

--- packages/bit-es/siapa/src/IdentityServiceProvider.php
+++ packages/bit-es/siapa/src/IdentityServiceProvider.php
@@ -1,49 +1,49 @@
 <?php
 
-declare(strict_types=1);
-
-namespace Rimba\Identity;
+namespace Bites\Identity;
 
 use Illuminate\Support\ServiceProvider;
-use Rimba\Identity\Managers\IdentityManager;
+use Filament\Panel;
 
 class IdentityServiceProvider extends ServiceProvider
 {
-    public function register(): void
+    public function register()
     {
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/identity.php',
-            'identity'
-        );
-
-        $this->app->singleton(
-            function (): IdentityManager {
-
-                $identityManager = new IdentityManager;
-
-                foreach (
-                    config('identity.drivers') as $name => $driver
-                ) {
-
-                    $identityManager->extend(
-                        $name,
-                        $driver
-                    );
-                }
-
-                return $identityManager;
-            }
-        );
+        $this->mergeConfigFrom(__DIR__.'/../config/identity.php', 'identity');
     }
 
-    public function boot(): void
+    public function boot()
     {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'bites-identity');
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+
         $this->publishes([
             __DIR__.'/../config/identity.php' => config_path('identity.php'),
-        ], 'identity-config');
+        ], 'config');
 
-        $this->loadMigrationsFrom(
-            __DIR__.'/../database/migrations'
-        );
+        $this->publishes([
+            __DIR__.'/../resources/assets/models' => public_path('models'),
+        ], 'assets');
+
+        if (config('identity.auto_register', true)) {
+            \Filament\Facades\Filament::serving(function () {
+                \Filament\Facades\Filament::registerPanelModification(function (Panel $panel) {
+                    $panel
+                        ->login(Pages\Auth\CustomLogin::class)
+                        ->registration(Pages\Auth\CustomRegister::class)
+                        ->passwordReset(Pages\Auth\CustomForgotPassword::class)
+                        ->profile(Pages\Profile::class)
+                        ->multiFactorAuthentication([
+                            \Filament\MultiFactorAuthentication\AppAuthentication::make()
+                                ->brandName(config('app.name'))
+                                ->recoverable(),
+                        ])
+                        ->middleware([
+                            \Bites\Identity\Http\Middleware\EnsureSetupIsComplete::class,
+                        ]);
+                });
+            });
+        }
     }
 }
