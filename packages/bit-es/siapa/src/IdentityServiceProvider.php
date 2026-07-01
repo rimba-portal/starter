33346e883fc6dfeee2108f19c804a39f0a732749
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-01 11:06:04 +0800

    Update IdentityServiceProvider.php

--- packages/bit-es/siapa/src/IdentityServiceProvider.php
+++ packages/bit-es/siapa/src/IdentityServiceProvider.php
@@ -26,24 +26,24 @@ class IdentityServiceProvider extends ServiceProvider
             __DIR__.'/../resources/assets/models' => public_path('models'),
         ], 'assets');
 
-        if (config('identity.auto_register', true)) {
-            \Filament\Facades\Filament::serving(function () {
-                \Filament\Facades\Filament::registerPanelModification(function (Panel $panel) {
-                    $panel
-                        ->login(Pages\Auth\Login::class)
-                        ->registration(Pages\Auth\Register::class)
-                        ->passwordReset(Pages\Auth\ForgotPassword::class)
-                        ->profile(Pages\Profile::class)
-                        ->multiFactorAuthentication([
-                            \Filament\Auth\MultiFactor\App\AppAuthentication::make()
-                                ->brandName(config('app.name'))
-                                ->recoverable(),
-                        ])
-                        ->middleware([
-                            \Bites\Identity\Http\Middleware\EnsureSetupIsComplete::class,
-                        ]);
-                });
-            });
-        }
+        // if (config('identity.auto_register', true)) {
+        //     \Filament\Facades\Filament::serving(function () {
+        //         \Filament\Facades\Filament::registerPanelModification(function (Panel $panel) {
+        //             $panel
+        //                 ->login(Pages\Auth\Login::class)
+        //                 ->registration(Pages\Auth\Register::class)
+        //                 ->passwordReset(Pages\Auth\ForgotPassword::class)
+        //                 ->profile(Pages\Profile::class)
+        //                 ->multiFactorAuthentication([
+        //                     \Filament\Auth\MultiFactor\App\AppAuthentication::make()
+        //                         ->brandName(config('app.name'))
+        //                         ->recoverable(),
+        //                 ])
+        //                 ->middleware([
+        //                     \Bites\Identity\Http\Middleware\EnsureSetupIsComplete::class,
+        //                 ]);
+        //         });
+        //     });
+        // }
     }
 }
